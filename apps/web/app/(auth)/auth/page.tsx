cc3b4692ea2f9d57a5ca748072929d1f6ec4cabb
ElormLokpo <benedictdev31@gmail.com>
2026-01-24 17:14:05 +0000

    minor changes

--- apps/web/app/(auth)/auth/page.tsx
+++ apps/web/app/(auth)/auth/page.tsx
@@ -4,23 +4,41 @@ import { authSchema, authSchemaType } from "@/schema";
 import { useState } from "react";
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
+import { CInput } from "@/components/shared/cinput";
 
 const AuthPage = () => {
     const [isLogin, useIsLogin] = useState(true);
-    const {handleSubmit} = useForm<authSchemaType>({
+    const { handleSubmit, register, formState: { errors } } = useForm<authSchemaType>({
         resolver: zodResolver(authSchema)
     })
 
 
-    const submitHandler = (data: authSchemaType)=>{
+    const submitHandler = (data: authSchemaType) => {
         console.log(data)
     }
 
     return (
-        <div className="h-screen bg-stone-900 text-stone-100 flex items-center justify-center">
-            <form onSubmit={()=>submitHandler(handleSubmit)}>
-                
-            </form>
+        <div className="h-screen flex justify-center items-center">
+            <div>
+                <form onSubmit={handleSubmit(submitHandler)} className="w-[18rem]">
+                    <div className="mb-2">
+                        <p className="tracking-[0.3rem] font-semibold text-stone-700 text-center">{isLogin ? "LOGIN" : "REGISTER"}</p>
+                    </div>
+
+                    <div className="mb-1.5">
+                        <CInput placeholder="Enter email" inputType="text" variant="auth" name="email" register={register} errors={errors} />
+                    </div>
+
+                    <div className="mb-2.5">
+                        <CInput placeholder="Enter password" inputType="text" variant="auth" name="password" fieldType="password" register={register} errors={errors} />
+                    </div>
+
+                    <div>
+                        {isLogin && <p className="text-xs underline hover:cursor-pointer">Forgot password?</p>}
+                    </div>
+
+                </form>
+            </div>
         </div>
     )
 }
--- apps/web/schema/index.tsx
+++ apps/web/schema/index.tsx
@@ -2,7 +2,7 @@ import z from "zod";
 
 
 export const authSchema = z.object({
-    username: z.string().min(1, { message: "Username is a required field." }),
+    email: z.email().min(1, { message: "Username is a required field." }),
     password: z.string().min(1, { message: "Password is a required field" })
 })
 
